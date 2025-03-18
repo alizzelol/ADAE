4e7d6dc73cb208e1e3dca42d941ba60da85b7b02
alizzelol <alizzelol@gmail.com>
2025-03-18 17:59:31 +0100

    Initial commit

---
+++ app/src/main/java/com/alizzelol/adae/MainActivity.java
@@ -0,0 +1,28 @@
+package com.alizzelol.adae;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRegistrarse = findViewById(R.id.btnRegistrarse);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnRegistrarse.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegistrarseActivity.class);
+            startActivity(intent);
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
